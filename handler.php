7d121e1614dd2b70326c009b0fab7e348316778b
IftekharApon <74828169+aponananna2020@users.noreply.github.com>
2021-02-11 14:30:06 +0600

    Update handler.php

--- handler.php
+++ handler.php
@@ -21,6 +21,6 @@ $validator->field('comments')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('someone@gmail.com'); // ← Your email here
+$pp->sendEmailTo('aponananna2000@gmail.com'); // ← Your email here
 
 echo $pp->process($_POST);
